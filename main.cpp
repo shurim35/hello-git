82ab2ae28b7e544158416ca7279f31af2aeeaf7d
unknown <shurim35@gmail.com>
2026-04-19 14:20:49 +0800

    Added for loop

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@ using namespace std;
 int main() {
     cout << "Hello, Git!" << endl;
     cout << "I love C++";
+
+    for (int i=1; i<=3 ; i++) {
+        cout << " Hello Git ! This is line " << i ;
+    }
+    
     return 0;
 }
